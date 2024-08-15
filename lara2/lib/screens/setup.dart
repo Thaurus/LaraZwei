0b50e612466fad8521b72d67e2f488bb285333e3
LaraZwei <nilsv-b@gmx.de>
2024-08-15 22:28:39 +0200

    neue Bilder

--- lara2/lib/screens/setup.dart
+++ lara2/lib/screens/setup.dart
@@ -5,7 +5,7 @@ String getChapterName(int index) {
 }
 
 Map<String, List<String>> images = {
-  "einfach": [
+  /*"einfach": [
     "affe",
     "hund",
     "lila",
@@ -50,5 +50,79 @@ Map<String, List<String>> images = {
     "fahrrad",
     "ballon",
     "hamster"
+  ],*/
+  "Kapitel 1": [
+    "Oma",
+    "Opa",
+    "Wal",
+    "Ufo",
+    "Hut",
+    "Hose",
+    "Hase",
+    "Nase"
   ],
+  "Kapitel 2": [
+    "Sofa",
+    "Brot",
+    "Lego",
+    "Kamel",
+    "Palme",
+    "Blume",
+    "Wolke",
+    "Pirat"
+  ],
+  "Kapitel 3 Sch": [
+    "Schaf",
+    "Schal",
+    "Schere",
+    "Schwan",
+    "Fisch",
+    "Tisch",
+    "Tasche",
+    "Dusche",
+    "Frosch",
+    "Flasche"
+  ],
+  "Kapitel 4 Au": [
+    "Auto",
+    "Auge",
+    "Baum",
+    "Raupe",
+    "Frau",
+    "Schaum",
+    "Maus",
+    "Zaun"
+  ],
+  "Kapitel 5 Ei": [
+    "Ei",
+    "Eis",
+    "Eimer",
+    "Bein",
+    "Drei",
+    "Leiter",
+    "Zwei",
+    "Schwein",
+    "Schleife",
+    "Leine"
+  ],
+  "Kapitel 8 Eu": [
+    "Eule",
+    "Neun",
+    "Euro",
+    "Kreuz",
+    "Feuer"
+  ],
+  "Kapitel 9 nk": [
+    "Bank",
+    "Anker",
+    "Schrank",
+    "Geschenk"
+  ],
+  "Kapitel 10 st": [
+    "Stein",
+    "Stift",
+    "Stelzen",
+    "Stempel",
+    "Stern"
+  ]
 };
