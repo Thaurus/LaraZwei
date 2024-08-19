003ca368423e563f80088674fc3a0f4a8aaf9a18
LaraZwei <nilsv-b@gmx.de>
2024-08-19 22:11:16 +0200

    responsive layout

--- lara2/lib/main.dart
+++ lara2/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
+import 'package:responsive_framework/utils/scroll_behavior.dart';
 import 'screens/home_screen.dart';
 
 void main() {
@@ -11,6 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget!),
+        breakpoints: const [
+          ResponsiveBreakpoint.resize(350, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(600, name: TABLET),
+          ResponsiveBreakpoint.autoScale(1200, name: DESKTOP),
+        ],
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
